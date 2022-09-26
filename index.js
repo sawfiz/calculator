91fe7c45ea0f95398d3b8052ec6106c2daba23ca
sawfiz <dashu.up@gmail.com>
2022-09-26 10:24:44 +0800

    General cleanup.  Add comments

--- index.js
+++ index.js
@@ -8,19 +8,21 @@ const deleteEl = document.querySelector("#delete");
 let display = "0";
 displayEl.innerText = display;
 
-let a = undefined;
-let b = undefined;
-let input = "";
-let operation = "";
-let decimal = false;
-let state = "waitForA";
+let a = undefined; // Operand A
+let b = undefined; // Operand B
+let input = ""; // Keypad input
+let operation = ""; // Operator
+let decimal = false; // Flag for indicating if decimal point has been used in a number
+let state = "waitForA"; // State indicator for the state machine
 
+// For each button, when click, animate a button press
 btnEls.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         animateButtonPress(e);
     });
 });
 
+// Getting number inputs from keypad
 numEls.forEach((num) => {
     num.addEventListener("click", () => {
         if (num.innerText === ".") {
@@ -36,9 +38,12 @@ numEls.forEach((num) => {
     });
 });
 
+// Get operand input from keypad
 opEls.forEach((op) => {
     op.addEventListener("click", () => {
+        // State machine of the calculator
         switch (state) {
+            // waitForA, wait for a valid Operand A to be entered
             case "waitForA":
                 if (input !== "") {
                     a = +input;
@@ -50,10 +55,12 @@ opEls.forEach((op) => {
                 }
                 animateDisplayBlink();
                 break;
+
+            // waitForB, already received a valid Operand A and an operator, wait for a valid operand B
             case "waitForB":
                 if (input !== "") {
                     b = +input;
-                    resetInput(); 
+                    resetInput();
                     result = calculate();
                     a = result;
                     displayEl.innerText = result;
@@ -63,6 +70,11 @@ opEls.forEach((op) => {
                     operation = op.innerText;
                 }
                 break;
+
+            // gotResult, got a valid calculation result.  Thress things can happen here:
+            // 1. a new number is entered with an new operator, this should be treated as Operand A
+            // 2. a "=" received, this should result a repeat of last calculation, with the last result as new Operand A
+            // 3. a "+ - * /" received, the last result should become the new Operand A, and go wait for B for calculation
             case "gotResult":
                 if (input !== "") {
                     // Received a new number, record it as A, go wait for B
@@ -92,11 +104,13 @@ opEls.forEach((op) => {
     });
 });
 
+// Utility function to consistently reset the input
 function resetInput() {
     input = "";
     decimal = false;
 }
 
+// Utility function to perform the calculations
 function calculate() {
     switch (operation) {
         case "+":
@@ -112,18 +126,18 @@ function calculate() {
     }
 }
 
+// The Clear button event listener
 clearEl.addEventListener("click", () => {
     a = 0;
     b = 0;
-    newNum = true;
-    display = "0";
+    resetInput();
     displayEl.innerText = display;
-    decimal = false;
 });
 
+// The Delete button event listener
 deleteEl.addEventListener("click", () => {
     if (input === "" || input.length === 1) {
-        input = "";
+        resetInput();
         displayEl.innerText = "0";
     } else {
         if (input[input.length - 1] === ".") {
@@ -134,18 +148,21 @@ deleteEl.addEventListener("click", () => {
     }
 });
 
+// Utility function to animate a button press
 async function animateButtonPress(e) {
     e.target.classList.add("active");
     await delay(100);
     e.target.classList.remove("active");
 }
 
+// Utility function to animate a display blink
 async function animateDisplayBlink() {
     displayEl.classList.add("active");
     await delay(100);
     displayEl.classList.remove("active");
 }
 
+// Utility function to create a delay
 function delay(time) {
     return new Promise((resolve) => setTimeout(resolve, time));
 }
