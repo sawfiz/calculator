7ccb2ef19007b26618ab6e25e218c678ea671983
sawfiz <dashu.up@gmail.com>
2022-11-16 19:16:18 +0800

    Modularize the code into 3 objects

--- index.js
+++ index.js
@@ -1,127 +1,99 @@
-const btnEls = Array.from(document.querySelectorAll('.btn'));
-const numEls = Array.from(document.querySelectorAll('.num'));
-const opEls = Array.from(document.querySelectorAll('.op'));
-const displayEl = document.querySelector('.display');
-const clearEl = document.querySelector('#clear');
-const deleteEl = document.querySelector('#delete');
+const display = (() => {
+  const displayEl = document.querySelector('.display');
 
-let a; // Operand A
-let b; // Operand B
-let input = ''; // Keypad input
-let operation; // Operator
-let decimal = false; // Flag for indicating if decimal point has been used in a number
-let state = 'waitForA'; // State indicator for the state machine
-let result;
-
-// Utility function to create a delay
-function delay(time) {
-  return new Promise((resolve) => setTimeout(resolve, time));
-}
-
-// this is no longer needed as I use CSS for the button press tranistion now
-// // Utility function to animate a button press
-// async function animateButtonPress(e) {
-//   e.target.classList.add('active');
-//   await delay(100);
-//   e.target.classList.remove('active');
-// }
-
-// Utility function to animate a display blink
-async function animateDisplayBlink() {
-  displayEl.classList.add('active');
-  await delay(100);
-  displayEl.classList.remove('active');
-}
-
-// Utility function to consistently reset the input
-function resetInput() {
-  input = '';
-  decimal = false;
-}
-
-// Utility function to perform the calculations
-function calculate() {
-  switch (operation) {
-    case '+':
-      return a + b;
-    case '-':
-      return a - b;
-    case '*':
-      return Math.round(a * b * 100000000) / 100000000;
-    case '/':
-      return Math.round(a / b * 100000000) / 100000000;
-    default:
-      return null;
+  // Utility function to create a delay
+  function delay(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
   }
-}
 
-function updateDisplay(string) {
-  const value = +string;
-
-  if (value > 999999999) {
-    displayEl.innerText = 'Too Large';
-    return;
+  // Utility function to animate a display blink to indicate a key's been pressed
+  async function animateDisplayBlink() {
+    displayEl.classList.add('active');
+    await delay(100);
+    displayEl.classList.remove('active');
   }
-  if (value < -99999999) {
-    displayEl.innerText = 'Too Small';
-    return;
-  }
-  if (value > 0 && value < 0.000001) {
-    displayEl.innerText = '0.00000000';
-    return;
-  }
-  if (value < 0 && value > -0.000001) {
-    displayEl.innerText = '0.00000000';
-    return;
-  }
-  displayEl.innerText = string.substring(0, 10);
-}
 
-// Start of the main program
-updateDisplay('0');
+  function updateDisplay(string) {
+    const value = +string;
 
-// this is no longer needed as I use CSS for the button press tranistion now
-// For each button, when click, animate a button press
-// btnEls.forEach((btn) => {
-//   btn.addEventListener('click', (e) => {
-//     animateButtonPress(e);
-//   });
-// });
-
-// Getting number inputs from keypad
-numEls.forEach((num) => {
-  num.addEventListener('click', () => {
-    if (num.innerText === '.') {
-      if (decimal === false) {
-        decimal = true;
-        input = input ? input + num.innerText : '0' + num.innerText;
-      } else {
-        animateDisplayBlink();
-      }
-    } else if (input === '0' || input === undefined) {
-      input = num.innerText;
-    } else {
-      input += num.innerText;
+    // Limit number to display within 10 digits
+    if (value > 9999999999) {
+      displayEl.innerText = 'Too Large';
+      return;
     }
-    // console.log('input: ', input);
-    updateDisplay(input);
-  });
-});
+    if (value < -999999999) {
+      displayEl.innerText = 'Too Small';
+      return;
+    }
+    if (value > 0 && value < 0.000001) {
+      displayEl.innerText = '0.00000000';
+      return;
+    }
+    if (value < 0 && value > -0.000001) {
+      displayEl.innerText = '0.00000000';
+      return;
+    }
+    displayEl.innerText = string.substring(0, 10);
+  }
 
-// Get operand input from keypad
-opEls.forEach((op) => {
-  op.addEventListener('click', () => {
-    // console.log(
-    //   `Got operator: ${op.innerText} state: ${state}  a: ${a}  b: ${b}  input: ${input}`
-    // );
-    animateDisplayBlink();
+  return { updateDisplay, animateDisplayBlink };
+})();
+display.updateDisplay('0');
+
+const core = (() => {
+  let a; // Operand A
+  let b; // Operand B
+  let operation; // Operator for calculation
+  let state = 'waitForA'; // State indicator for the state machine
+  let result;
+
+  // Declare and initialize these variables
+  let input = ''; // Keypad input
+  let decimal = false; // Flag for indicating if decimal point has been used in a number
+
+  // Utility function to consistently reset the input
+  function resetInput() {
+    input = '';
+    decimal = false;
+  }
+
+  function convertOp(op) {
+    operation = op.innerText;
+    if (op.id === 'divide') operation = '/'; // Convert ÷ to /
+    if (op.id === 'multiply') operation = '*'; // Conver ᳵ to *
+  }
+
+  // Utility function to perform the calculations
+  function calculate() {
+    console.log(a, operation, b);
+
+    switch (operation) {
+      case '+':
+        return a + b;
+      case '-':
+        return a - b;
+      case '*':
+        return Math.round(a * b * 100000000) / 100000000;
+      case '/':
+        return Math.round((a / b) * 100000000) / 100000000;
+      default:
+        return null;
+    }
+  }
+
+  function processOp(op) {
+    display.animateDisplayBlink();
+    console.log(
+      `Got operator: ${op.innerText} state: ${state}  a: ${a}  b: ${b}  input: ${input}`
+    );
+
     // State machine of the calculator
     switch (state) {
       // waitForA, wait for a valid Operand A to be entered
       case 'waitForA':
         // Recived an operator, but there is no input, stay in waiForA
         if (input === '') {
-          animateDisplayBlink();
+          display.animateDisplayBlink();
           state = 'waitForA';
           break;
         }
@@ -129,13 +101,13 @@ opEls.forEach((op) => {
           // Received an operator, and there is an input
           if (op.innerText === '=') {
             // The operator is '=', stay in waitForA
-            animateDisplayBlink();
+            display.animateDisplayBlink();
             state = 'waitForA';
             break;
           } else {
             // The operator is one of '+-*/', register A and op, move to waitForB
             a = +input;
-            operation = op.innerText;
+            convertOp(op);
             resetInput();
             state = 'waitForB';
           }
@@ -146,26 +118,22 @@ opEls.forEach((op) => {
       case 'waitForB':
         // Recived an operator, but there is no input, stay in waiForA
         if (input === '') {
-          operation = op.innerText;
-          animateDisplayBlink();
+          convertOp(op);
+          display.animateDisplayBlink();
           break;
         }
-
-        // console.log(
-        //   `Got operator: ${op.innerText} state: ${state}  a: ${a}  b: ${b}  input: ${input}`
-        // );
         // Received an operator and there is an input, calculate
         if (input !== '') {
           b = +input;
-          resetInput();
           result = calculate();
+          display.updateDisplay(result.toString());
+          resetInput();
           a = result;
-          updateDisplay(result.toString());
           if (op.innerText === '=') {
             state = 'gotResult';
             break;
           } else {
-            operation = op.innerText;
+            convertOp(op);
             state = 'waitForB';
           }
         }
@@ -178,7 +146,7 @@ opEls.forEach((op) => {
             // No input, repeat last calculation, stay in this state
             result = calculate();
             a = result;
-            updateDisplay(result.toString());
+            display.updateDisplay(result.toString());
             break;
           } else {
             // There is an input, go to waitForA for user to complete the input
@@ -188,13 +156,13 @@ opEls.forEach((op) => {
         } else if (input === '') {
           // Recived '+-*/'
           // Got no input, register the operator, use current result as A, go to waitForB
-          operation = op.innerText;
+          convertOp(op);
           state = 'waitForB';
           break;
         } else {
           // Got an input, register as A, register the operator, go to waitForB
           a = +input;
-          operation = op.innerText;
+          convertOp(op);
           resetInput();
           state = 'waitForB';
         }
@@ -204,27 +172,89 @@ opEls.forEach((op) => {
         break;
     }
     // console.log(`state: ${state}  a: ${a}  b: ${b}  input: ${input}`);
-  });
-});
-
-// The Clear button event listener
-clearEl.addEventListener('click', () => {
-  a = 0;
-  b = 0;
-  resetInput();
-  updateDisplay('0');
-});
-
-// The Delete button event listener
-deleteEl.addEventListener('click', () => {
-  if (input === '' || input.length === 1) {
-    resetInput();
-    updateDisplay('0');
-  } else {
-    if (input[input.length - 1] === '.') {
-      decimal = false;
-    }
-    input = input.substring(0, input.length - 1);
-    updateDisplay(input);
   }
-});
+
+  //
+  function processNum(number) {
+    if (number === '.') {
+      if (decimal === false) {
+        // A decimal number that starts with ., add 0 in front
+        decimal = true;
+        // eslint-disable-next-line prefer-template
+        input = input ? input + number : '0' + number;
+      } else {
+        // Ignore if the number already had a decimal point
+        display.animateDisplayBlink();
+      }
+    } else if (input === '0' || input === undefined) {
+      // Ignore 0 if it is the first char of a number
+      input = number;
+    } else {
+      input += number;
+    }
+    console.log('input: ', input);
+    display.updateDisplay(input);
+  }
+
+  function processDel() {
+    // Special case: there is nothing to delete
+    if (input === '' || input.length === 1) {
+      resetInput();
+      display.updateDisplay('0');
+    } else {
+      // Normal case: there is something to delete
+      // Special case: last character is a decimal point
+      if (input[input.length - 1] === '.') {
+        decimal = false;
+      }
+      // Delete the last character of the input
+      input = input.substring(0, input.length - 1);
+      display.updateDisplay(input);
+    }
+  }
+
+  function processClear() {
+    a = 0;
+    b = 0;
+    resetInput();
+    display.updateDisplay('0');
+  }
+
+  return {
+    processNum,
+    processOp,
+    processClear,
+    processDel,
+  };
+})();
+
+const clickHandler = (() => {
+  const numEls = Array.from(document.querySelectorAll('.num'));
+  const opEls = Array.from(document.querySelectorAll('.op'));
+  const clearEl = document.querySelector('#clear');
+  const deleteEl = document.querySelector('#delete');
+
+  // Getting number inputs from keypad
+  numEls.forEach((num) => {
+    num.addEventListener('click', () => {
+      core.processNum(num.innerText);
+    });
+  });
+
+  // Get operand input from keypad
+  opEls.forEach((op) => {
+    op.addEventListener('click', () => {
+      core.processOp(op);
+    });
+  });
+
+  // The Clear button event listener
+  clearEl.addEventListener('click', () => {
+    core.processClear();
+  });
+
+  // The Delete button event listener
+  deleteEl.addEventListener('click', () => {
+    core.processDel();
+  });
+})();
